e965667db21b14480c2d23f42eca0a5628710d01
ali koyuncu <alikoyuncu13@gmail.com>
2021-09-22 01:54:54 +0300

    eklendi2

---
+++ cdersleri/cdersleri/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ShortTipi
+{
+    class program1{
+
+        static void Main(string[] args)
+        {
+
+            int bakiye = 1000;
+            Console.WriteLine(" ATM ye Hoşgeldiniz");
+            Console.WriteLine(" Yapmak istediğiniz işlemi seçiniz");
+            Console.WriteLine("1 - Bakiye öğrenme");
+            Console.WriteLine("2 - Para Çekme");
+            Console.WriteLine("3 - Para Yatırma");
+            Console.WriteLine("q - Sistemden Ayrılma");
+            //Console.ReadLine();
+            String islem = Console.ReadLine();
+
+            if (islem=="1")
+            {
+                Console.WriteLine("Şu anki bakiyeniz : " + bakiye);
+                Console.ReadLine();
+
+            }
+            else if (islem=="2")
+            {
+                Console.WriteLine("Çekmek istediğiniz tutarı giriniz ?");
+               
+
+                int cekilecek_tutar = Convert.ToInt32(Console.ReadLine());
+                if(cekilecek_tutar >= bakiye || cekilecek_tutar <= 0)
+                {
+                    Console.WriteLine("Hatalı tutar girdiniz");
+                }
+                else
+                {
+                    bakiye -= cekilecek_tutar;
+                    Console.WriteLine("Kalan tutarızınız : " + bakiye);
+                    Console.ReadLine();
+                }
+               
+            }
+            else if (islem=="3")
+            {
+                Console.WriteLine("Yatırmak istediğiniz tutarı giriniz");
+           
+                int yatırılacak_tutar = Convert.ToInt32(Console.ReadLine());
+                if(yatırılacak_tutar>=0)
+                {
+                    bakiye += yatırılacak_tutar;
+                    Console.WriteLine("Yeni bakiyeniz : " + bakiye);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli bir bakiye giriniz");
+                }
+                
+            }
+            else if(islem=="q")
+            {
+                Console.WriteLine("ATM den çıkış yapılıyor, yine bekleriz");
+                Console.WriteLine("Çıkış yapıldı");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir işlem seçiniz");
+                Console.ReadLine();
+            }
+
+
+
+        }
+
+    }
+}
